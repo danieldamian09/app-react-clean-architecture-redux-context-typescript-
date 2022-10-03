111b9356ad4c97862887687c89a5e66c181eddde
danieldamian09 <humbertorivero9@gmail.com>
2022-10-03 20:15:58 -0300

    Add DataGrid component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-
+import { People } from '@/data';
+import { DataGrid, GridRenderCellParams, } from "@mui/x-data-grid";
+import React from "react";
 
 export interface HomeInterface {}
 
-const Home : React.FC<HomeInterface> = () => {
-	return <div>Home</div>;
+const Home: React.FC<HomeInterface> = () => {
+
+	const pageSize = 5;
+
+	const columns = [
+		{
+			field: "name",
+			headerName: "Name",
+			flex: 1,
+			minWidth: 150,
+			renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+		}
+	];
+
+	return (
+		<div>
+			<DataGrid
+				rows={People}
+				columns={columns}
+				disableColumnSelector
+				disableSelectionOnClick
+				autoHeight
+				pageSize={pageSize}
+				rowsPerPageOptions={[pageSize]}
+			/>
+		</div>
+	);
 };
 
 export default Home;
